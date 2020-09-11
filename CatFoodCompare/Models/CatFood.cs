cee5c4a6c4d47c7fd141eca2f6b91d8a095a61c5
Joshua Lovelace <rakmial@gmail.com>
2020-09-11 13:20:37 -0700

    test 1 properties green

--- CatFoodCompare/Models/CatFood.cs
+++ CatFoodCompare/Models/CatFood.cs
@@ -1,8 +1,27 @@
+using System.Collections.Generic;
 
 namespace CatFoodCompare.Models
 {
   public class CatFood
   {
-    // stuff
+    public string Name { get; }
+    public string Brand { get; }
+    public double Price { get; }
+    public int KCalKG { get; }
+    public int UnitMassG { get; }
+    public List<string> Ingredients { get; }
+    public Dictionary<string, int> GuaranteedAnalysis { get; }
+
+    public CatFood(string name, string brand, double price, int kCalKG, int unitMassG, 
+      List<string> ingredients, Dictionary<string, int> guaranteedAnalysis)
+    {
+        Name = name;
+        Brand = brand;
+        Price = price;
+        KCalKG = kCalKG;
+        UnitMassG = unitMassG;
+        Ingredients = ingredients;
+        GuaranteedAnalysis = guaranteedAnalysis;
+    }
   }
 }
