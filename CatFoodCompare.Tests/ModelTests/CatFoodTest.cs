f5c2ed1a55590d890e2c859dd6fb4cfdc070aa24
Joshua Lovelace <rakmial@gmail.com>
2020-09-11 15:41:54 -0700

    test 5 ComparePrice class method red

--- CatFoodCompare.Tests/ModelTests/CatFoodTest.cs
+++ CatFoodCompare.Tests/ModelTests/CatFoodTest.cs
@@ -12,6 +12,7 @@ namespace CatFoodCompare.Tests
     //{
     //  CatFood.ClearAll();
     //}
+    // Test Food Object 1
     static string testName = "Chicken & Salmon Rustic Blend";
     static string testBrand = "Open Farm";
     static float testPrice = 2.19F;
@@ -29,6 +30,25 @@ namespace CatFoodCompare.Tests
 
     CatFood testFood = new CatFood(testName, testBrand, testPrice, testKCalKG,
       testUnitMassG, testIngredients, testGuaranteedAnalysis);
+
+    // Test Food Object 2
+    static string testName2 = "Chicken & Tuna Dinner";
+    static string testBrand2 = "Friskies";
+    static float testPrice2 = 0.85F;
+    static int testKCalKG2 = 1244;
+    static int testUnitMassG2 = 156;
+
+    static List<string> testIngredients2 = new List<string> {
+      "Meat By-products", "Poultry By-products", "Water", "Chicken", "Liver", "Tuna", "Rice", 
+      "Artificial and Natural Flavors", "Added Color", "Guar Gum"
+      };
+
+    static Dictionary<string, int> testGuaranteedAnalysis2 = new Dictionary<string, int> {
+      {"Crude Protein", 10}, {"Crude Fat", 5}, {"Crude Fiber", 1}, {"Moisture", 78}};
+
+    CatFood testFood2 = new CatFood(testName2, testBrand2, testPrice2, testKCalKG2,
+      testUnitMassG2, testIngredients2, testGuaranteedAnalysis2);
+
 
     [TestMethod]
     public void CatFoodConstructor_CreatesInstanceOfCatFood_CatFood()
@@ -60,6 +80,17 @@ namespace CatFoodCompare.Tests
     {
       float testKCalToPrice = testKCalKG * (testUnitMassG / 1000) / testPrice;
       Assert.AreEqual(testKCalToPrice, testFood.KCalPrice);
+    }
+    [TestMethod]
+    public void ComparePrice_ReturnsFormattedStringOfBrandNameAndKCalPrice_String()
+    {
+      List<CatFood> CatFoodList = new List<CatFood> {testFood, testFood2};
+      string testFormattedCompare = "";
+      foreach (CatFood food in CatFoodList)
+      {
+        testFormattedCompare += String.Format("{0} {1}: {0:C2}", food.Brand, food.Name, food.KCalPrice);
+      }
+      Assert.AreEqual(testFormattedCompare, CatFood.ComparePrice());
     }
     // [TestMethod]
     // public void MethodTested_Behavior_ExpectedResult()
